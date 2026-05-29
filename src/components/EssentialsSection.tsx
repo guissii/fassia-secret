f8b0e79681ef27ea1f289d4a7ea53fed755d8185
guissii <guissimohammed04@gmail.com>
2026-05-29 13:52:51 +0100

    fix: supprime banniere de la section essentiels

--- src/components/EssentialsSection.tsx
+++ src/components/EssentialsSection.tsx
@@ -1,14 +1,10 @@
 import { CollectionCarousel } from './CollectionCarousel';
 
-import { LOCAL_BANNERS } from '../lib/bannersConfig';
-
 export function EssentialsSection({ products }: { products: any[] }) {
-  const bannerUrl = LOCAL_BANNERS.essentials;
-
   return (
     <CollectionCarousel 
       title="ESSENTIELS"
-      imageSrc={bannerUrl}
+      imageSrc=""
       products={products}
       linkHref="/boutique"
       linkTitle="Découvrir tous nos essentiels"
